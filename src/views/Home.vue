9adc7753e73166708a82b308836b30ca2c6ebe13
mug1wara26 <aloysiusgsq@gmail.com>
2023-10-06 01:00:29 +0800

    fixed section clipping issues

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <v-container fluid>
     <h1 id="home" class="anchor"></h1>
     <div style="padding-top: 10%">
-      <h1 :class="{mobile: isMobile, 'not-mobile': !isMobile}">SP Tetris Open Invitational</h1>
+      <h1 class="responsive">SP Tetris Open Invitational</h1>
       <h2>
         Saturday, 4 November 2023 <br/>
         1100-1800 GMT+8
@@ -22,13 +22,33 @@
 </template>
 
 <style scoped>
-h1.mobile {
-  font-size: 72px;
-}
-h1.not-mobile {
-  font-size: 80px;
+
+@media screen and (max-width: 359px) {
+  h1.responsive {
+    font-size: 48px;
+  }
+  html {
+    font-size: 24px;
+  }
 }
 
+@media screen and (min-width: 360px) and (max-width: 439px) {
+  h1.responsive {
+    font-size: 64px;
+  }
+}
+
+@media screen and (min-width: 400px) and (max-width: 639px) {
+  h1.responsive {
+    font-size: 72px;
+  }
+}
+
+@media screen and (min-width: 640px) {
+  h1.responsive {
+    font-size: 80px;
+  }
+}
 </style>
 
 <script lang="ts">
