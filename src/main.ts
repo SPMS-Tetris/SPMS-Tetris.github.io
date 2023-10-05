9333566819ffc6a26733e8aebb02bf495c13bddf
mug1wara26 <aloysiusgsq@gmail.com>
2023-10-05 23:57:53 +0800

    added title meta tag

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import VueMeta from "vue-meta";
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
@@ -10,3 +11,5 @@ new Vue({
   vuetify,
   render: h => h(App)
 }).$mount('#app')
+
+Vue.use(VueMeta)
