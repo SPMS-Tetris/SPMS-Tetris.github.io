05f03615e970250a7acd6c326eb4bdd29359a5ca
mug1wara26 <aloysiusgsq@gmail.com>
2023-10-07 10:44:06 +0800

    challonge embeds are real now

--- src/views/Brackets.vue
+++ src/views/Brackets.vue
@@ -2,20 +2,21 @@
   <v-container fluid>
     <h1 id="live bracket" class="anchor"/>
     <h1>Live Brackets</h1>
+    <h2> You can view the event here at our <a href="https://challonge.com/events/spopentetris2023" target="_blank">Challonge Page</a></h2> <br/>
 
     <v-container fluid class="pa-2 elevation-6 bracket">
       <h1 style="">Gold</h1>
-      <iframe src="https://challonge.com/SPMST_Session/module" width="100%" height="500" frameborder="0" scrolling="auto" allowtransparency="true"></iframe>
+      <iframe src="https://challonge.com/spopentetris2023_gold/module" width="100%" height="500" frameborder="0" scrolling="auto" allowtransparency="true"></iframe>
     </v-container>
 
     <v-container fluid class="pa-2 elevation-6 mt-12 bracket">
       <h1 style="">Platinum</h1>
-      <iframe src="https://challonge.com/SPMST_Session/module" width="100%" height="500" frameborder="0" scrolling="auto" allowtransparency="true"></iframe>
+      <iframe src="https://challonge.com/spopentetris2023_platinum/module" width="100%" height="500" frameborder="0" scrolling="auto" allowtransparency="true"></iframe>
     </v-container>
 
     <v-container fluid class="pa-2 elevation-6 mt-12 bracket">
       <h1 style="">Diamond</h1>
-      <iframe src="https://challonge.com/SPMST_Session/module" width="100%" height="500" frameborder="0" scrolling="auto" allowtransparency="true"></iframe>
+      <iframe src="https://challonge.com/spopentetris2023_diamond/module" width="100%" height="500" frameborder="0" scrolling="auto" allowtransparency="true"></iframe>
     </v-container>
   </v-container>
 </template>
