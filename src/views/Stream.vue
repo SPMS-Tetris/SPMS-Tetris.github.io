6e28b05b1468dbaaef94e3d5247aff1d881c823b
mug1wara26 <aloysiusgsq@gmail.com>
2023-10-07 11:13:04 +0800

    stream div responsive

--- src/views/Stream.vue
+++ src/views/Stream.vue
@@ -4,14 +4,15 @@
     <h1>Stream</h1>
 
 
-    <iframe
-        src="https://player.twitch.tv/?channel=spmstetris&parent=spms-tetris.github.io&muted=true"
-        width="100%"
-        height="720"
-        allowfullscreen
-        class="stream"
-    >
-    </iframe>
+    <v-container fluid class="pa-2 elevation-6 mt-12 stream-container">
+      <iframe
+          src="https://player.twitch.tv/?channel=spmstetris&parent=spms-tetris.github.io&muted=true"
+          width="100%"
+          height="720"
+          allowfullscreen
+          class="stream"
+      />
+    </v-container>
   </v-container>
 </template>
 
@@ -26,13 +27,28 @@
   background-color: #fffdd0
 }
 
+@media only screen and (max-width: 480px) {
+  .stream {
+    height: 300px;
+  }
+}
+
+@media only screen and (min-width: 481px) and (max-width: 640px) {
+  .stream {
+    height: 480px;
+  }
+}
+
 
 @media only screen and (max-width: 640px) {
   .stream-container {
     width: 100%;
   }
+}
+
+@media screen and (min-width: 641px) and (max-width: 960px){
   .stream {
-    height: 480px;
+    height: 640px;
   }
 }
 
